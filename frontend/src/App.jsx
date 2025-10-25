d53073c0a32202a94231757d0d1e0d26126389ef
Felipe Borges <felipetecacc@gmail.com>
2025-10-25 02:42:39 -0300

    feat: create navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
 import Collection from './pages/Collection';
@@ -10,10 +10,12 @@ import Cart from './pages/Cart';
 import Login from './pages/Login';
 import PlaceOrder from './pages/PlaceOrder';
 import Orders from './pages/Orders';
+import Navbar from './components/Navbar';
 
 const App = () => {
   return ( 
     <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+      <Navbar />
       <Routes>
         <Route path='/' element={<Home/>} />
         <Route path='/collection' element={<Collection/>} />
