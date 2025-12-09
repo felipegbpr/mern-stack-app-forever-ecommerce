92838070bd898b4bc0aff7efff35cb3960533993
Felipe Borges <felipetecacc@gmail.com>
2025-12-09 16:07:51 -0300

    feat: aggregate home page components

--- frontend/src/components/BestSeller.jsx
+++ frontend/src/components/BestSeller.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react';
 import { ShopContext } from '../context/ShopContext';
 import Title from './Title';
 import ProductItem from './ProductItem';
@@ -23,11 +23,15 @@ const BestSeller = () => {
       </div>
 
       <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6'>
-        {
-            bestSeller.map((item, index) => (
-                <ProductItem key={index} id={item._id} name={item.name} image={item.image} price={item.price} />
-            ))
-        }
+        {bestSeller.map((item, index) => (
+          <ProductItem
+            key={index}
+            id={item._id}
+            name={item.name}
+            image={item.image}
+            price={item.price}
+          />
+        ))}
       </div>
     </div>
   );
