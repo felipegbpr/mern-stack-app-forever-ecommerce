8faa4a6ea00cba8198cd3766f735c1a80d5c5597
Felipe Borges <felipetecacc@gmail.com>
2026-01-04 17:37:56 -0300

    feat: insert pages structure

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Login = () => {
-  return <div></div>;
+
+  const [currentState, setCurrentState] = useState('Sign Up');
+
+  return (
+    <form className='flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 gap-4 text-gray-800'>
+      <div className='inline-flex items-center gap-2 mb-2 mt-10'>
+        <p>{currentState}</p>
+      </div>
+    </form>
+  );
 };
 
 export default Login;
