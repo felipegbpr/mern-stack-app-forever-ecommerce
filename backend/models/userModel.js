d49ecc25196e734026ec13811e06519506042d8a
Felipe Borges <felipetecacc@gmail.com>
2026-02-22 15:14:37 -0300

    feat: connect backend apis with ecommerce website

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -5,7 +5,7 @@ const userSchema = new mongoose.Schema(
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    cartData: { type: Object, defult: {} },
+    cartData: { type: Object, default: {} },
   },
   { minimize: false }
 );
