8c41e2b184f62a4234a3316e9bd8764ddea6e289
Felipe Borges <felipetecacc@gmail.com>
2026-03-01 17:28:13 -0300

    fix: adjust order items list

--- frontend/src/pages/Orders.jsx
+++ frontend/src/pages/Orders.jsx
@@ -43,7 +43,7 @@ const Orders = () => {
       </div>
 
       <div>
-        {orderData.slice(1, 4).map((item, index) => (
+        {orderData.map((item, index) => (
           <div
             key={index}
             className='py-4 border-t border-b text-gray-700 flex flex-col md:flex-row md:items-center md:justify-between gap-4'
